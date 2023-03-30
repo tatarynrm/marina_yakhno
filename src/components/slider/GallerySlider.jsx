e5bedb8eece4b45158d07f2485ce9ee2e15457e0
tatarynrm <tatarynrm@gmail.com>
2023-03-30 19:31:50 +0300

    all done

--- src/components/slider/GallerySlider.jsx
+++ src/components/slider/GallerySlider.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import image1 from "../../assets/images/slider/1.jpg";
+// import image1 from "../../assets/images/slider/1.jpg";
 import image2 from "../../assets/images/slider/2.jpg";
 import image3 from "../../assets/images/slider/3.jpg";
 import image4 from "../../assets/images/slider/4.jpg";
+import image10 from "../../assets/images/slider/10.jpg";
+import image11 from "../../assets/images/slider/11.jpg";
+import image12 from "../../assets/images/slider/12.jpg";
+import image13 from "../../assets/images/slider/13.jpg";
+import image14 from "../../assets/images/slider/14.jpg";
 
 const sliderItems = [
-  { img: image1, text: "Порошкове фарбування металу" },
+  // { img: image1, text: "Порошкове фарбування металу" },
   { img: image2, text: "Корпуси терміналу самообслуговювання" },
   { img: image3, text: "Self Service Box B" },
   { img: image4, text: "Self Service Box A" },
+  { img: image10, text: "Приклад викраски" },
+  { img: image11, text: "Приклад викраски" },
+  { img: image12, text: "Приклад викраски" },
+  { img: image13, text: "Приклад викраски" },
+  { img: image14, text: "Приклад викраски" },
 ];
 
 export default function GallerySlider() {
@@ -22,7 +32,7 @@ export default function GallerySlider() {
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1386,
